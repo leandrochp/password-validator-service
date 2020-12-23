ddfa1a66866df5c84ed2dc14c91112e5214b3c26
Leandro Pereira <leandrochp@gmail.com>
2020-12-23 16:09:08 -0300

    feature: add start simple http server (Vert.x)

---
+++ src/main/kotlin/io/github/leandro/passw0rdvalidatorservice/application/Application.kt
@@ -0,0 +1,26 @@
+package io.github.leandro.passw0rdvalidatorservice.application
+
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+
+object Application {
+
+    fun start() {
+        val vertx = Vertx.vertx()
+        val router = Router.router(vertx)
+        val server = vertx.createHttpServer()
+
+        router.get("/").handler {
+            it.response().end("Welcome to Vert.x HTTP Server")
+        }
+
+        server.requestHandler(router).listen(8080) {
+            if (it.failed()) {
+                println("HTTP Server error ${it.cause()}")
+            } else {
+                println("HTTP Server started on port 8080")
+            }
+        }
+    }
+}
+
---
+++ src/main/kotlin/io/github/leandro/passw0rdvalidatorservice/application/Main.kt
@@ -0,0 +1,9 @@
+package io.github.leandro.passw0rdvalidatorservice.application
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Application.start()
+    }
+}
