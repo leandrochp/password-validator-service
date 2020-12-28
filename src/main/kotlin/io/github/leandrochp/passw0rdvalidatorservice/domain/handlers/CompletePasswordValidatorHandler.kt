bcebd2f0ecc347ae269aeba241f3ddfcfc176dc5
Leandro Pereira <leandrochp@gmail.com>
2020-12-27 22:39:38 -0300

    feature: add handler from complete password validator

---
+++ src/main/kotlin/io/github/leandrochp/passw0rdvalidatorservice/domain/handlers/CompletePasswordValidatorHandler.kt
@@ -0,0 +1,16 @@
+package io.github.leandrochp.passw0rdvalidatorservice.domain.handlers
+
+import io.github.leandrochp.passw0rdvalidatorservice.domain.password.Password
+import io.github.leandrochp.passw0rdvalidatorservice.domain.validators.Validator
+
+class CompletePasswordValidatorHandler(
+    private val validators: List<Validator<Password>>
+) {
+
+    fun check(password: Password): Boolean {
+        validators.map {
+            if (!it.validate(password)) return false
+        }
+        return true
+    }
+}
